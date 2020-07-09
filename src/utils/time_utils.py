e1c54e672ef0474e56256d2e43943a2a62a7d059
Tales Pimentel <tales.tsp@gmail.com>
2020-07-08 21:14:14 -0300

    Cleaning time_utils.py

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -14,12 +14,6 @@ def str_to_datetime(str_dt, str_dt_format='%d/%m/%Y %H:%M:%S'):
     use_str_dt = str_dt.split(".")[0]
     return datetime.datetime.strptime(use_str_dt, str_dt_format)
 
-def str_to_datetime_try_formats(str_dt):
-    try:
-        return str_to_datetime(str_dt, str_dt_format='%Y-%m-%d %H:%M:%S')
-    except ValueError:
-        return str_to_datetime(str_dt, str_dt_format='%Y/%m/%d %H:%M:%S')
-
 def time_resolution(data):
     dates = data["Date"].drop_duplicates().apply(pd.to_datetime).sort_values()
     return dates.iloc[1: len(dates) - 1].reset_index(drop=True) - dates.iloc[0: len(dates) - 2].reset_index(
@@ -63,33 +57,6 @@ def str_datetime_to_timestamp(str_datetime, str_dt_format='%Y-%m-%d %H:%M:%S'):
 
 def timestamp_to_datetime(ts):
     return datetime.datetime.fromtimestamp(ts)
-    
+
 def delta_time_hour(str_dt_initial, str_dt_final, str_dt_format='%Y-%m-%d %H:%M:%S'):
     return delta_time_seconds(str_dt_initial, str_dt_final, str_dt_format=str_dt_format) / HOUR_SECONDS
-
-def dt_to_str(dt, str_format):
-    return dt.strftime(str_format)
-
-def str_datetime_n_days_before(str_dt, n_delta_days, str_dt_format="%d/%m/%Y"):
-    if n_delta_days == 0:
-        return str_dt
-    else:
-        dt_n_days_before = str_to_datetime(str_dt, str_dt_format) + datetime.timedelta(days=n_delta_days)
-        return dt_n_days_before.strftime(str_dt_format)
-
-def is_commerce_time(h, m):
-    if h >= 8 and h <= 17:
-        result = True
-    elif h == 18 and m == 0:
-        result = True
-    else:
-        result = False
-
-    return result
-
-def calc_n_days_delta_date(date_str, n_days_list, str_dt_format="%d-%m-%Y"):
-    dates_delta = []
-    for n_day_delta in n_days_list:
-        date_delta = str_datetime_n_days_before(date_str, n_day_delta, str_dt_format=str_dt_format)
-        dates_delta.append(date_delta)
-    return dates_delta
