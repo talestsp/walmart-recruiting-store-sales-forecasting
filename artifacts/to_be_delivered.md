0853fc6a9862a07222414e70e87a97fb5cb9e3ef
Tales Pimentel <tales.tsp@gmail.com>
2020-07-10 06:39:17 -0300

    final code

--- artifacts/to_be_delivered.md
+++ artifacts/to_be_delivered.md
@@ -1,3 +1,10 @@
 https://www.kaggle.com/c/walmart-recruiting-store-sales-forecasting
 
-We expect you to create a Kaggle notebook (https://www.kaggle.com/docs/kernels) with your solution, explaining how you approached the problem presented in the challenge. It is important to describe which decisions you have made and the rationale behind each. Also, you should explain how you cleaned and processed the data, which algorithms you have used, the strategy you applied on feature engineering and everything else you assume is important for us to understand your solution.
+## What you should provide us
+We expect you to create a Kaggle notebook (https://www.kaggle.com/docs/kernels) 
+with your solution, explaining how you approached the problem presented in the 
+challenge. It is important to describe which decisions you have made and the 
+rationale behind each. Also, you should explain how you cleaned and processed 
+the data, which algorithms you have used, the strategy you applied on feature 
+engineering and everything else you assume is important for us to understand 
+your solution.
--- src/processment/process.py
+++ src/processment/process.py
@@ -81,3 +81,146 @@ def train_sales_semantic_enrichment(data):
 
     return data
 
+
+def wm_data_BKP(data):
+    transformed_data = []
+
+    for (store_dept, wm_date), g in data.groupby(["store_dept", "wm_date"]):
+        sorted_group = g[["year", "Weekly_Sales", "Size", "Store", "Dept", "Date", "IsHoliday"]].sort_values("year")
+        store = sorted_group["Store"].iloc[0]
+        dept = sorted_group["Dept"].iloc[0]
+        date = sorted_group["Date"].iloc[0]
+        is_holiday = sorted_group["IsHoliday"].iloc[0]
+
+        transformed_data_row = {"store_dept": store_dept, "wm_date": wm_date,
+                              "Store": store, "Dept": dept, "Date": date, "IsHoliday": is_holiday}
+
+        for year_i in range(len(sorted_group)):
+            year_n_label = "year" + str(year_i)
+            year_sales_label = year_n_label + "_sales"
+            size_n_label = year_n_label + "_size"
+
+            year_value = sorted_group.iloc[year_i]["year"]
+            x_value = sorted_group.iloc[year_i]["Weekly_Sales"]
+            size_value = sorted_group.iloc[year_i]["Size"]
+
+            transformed_data_row[year_n_label] = year_value
+            transformed_data_row[year_sales_label] = x_value
+            transformed_data_row[size_n_label] = size_value
+
+        transformed_data.append(transformed_data_row)
+
+    return pd.DataFrame(transformed_data)
+
+
+def wm_data(data):
+    transformed_data = []
+
+    for (store_dept, wm_date), g in data.groupby(["store_dept", "wm_date"]):
+        sorted_group = g[["year", "Weekly_Sales", "Size", "Store", "Dept", "Date", "IsHoliday",
+                          "MarkDown1", "MarkDown2", "MarkDown3", "MarkDown4", "MarkDown5",
+                          "Fuel_Price", "CPI", "Unemployment", "week_n"]].sort_values("year")
+
+        store = sorted_group["Store"].iloc[0]
+        dept = sorted_group["Dept"].iloc[0]
+        date = sorted_group["Date"].iloc[0]
+        #         is_holiday = sorted_group["IsHoliday"].iloc[0]
+
+        transformed_data_row = {"store_dept": store_dept, "wm_date": wm_date,
+                              "Store": store, "Dept": dept, "Date": date}
+
+        for year_i in range(len(sorted_group)):
+            year_n_label = "year" + str(year_i)
+            year_n_sales_label = year_n_label + "_sales"
+            size_n_label = year_n_label + "_size"
+            isholiday_n_label = year_n_label + "_isholiday"
+            fuel_price_n_label = year_n_label + "_fuel_price"
+            cpi_n_label = year_n_label + "_cpi"
+            unempl_n_label = year_n_label + "_unempl"
+            week_n_n_label = year_n_label + "_week_n"
+            md1_n_label = year_n_label + "_md1"
+            md2_n_label = year_n_label + "_md2"
+            md3_n_label = year_n_label + "_md3"
+            md4_n_label = year_n_label + "_md4"
+            md5_n_label = year_n_label + "_md5"
+
+            year_value = sorted_group.iloc[year_i]["year"] - 2010
+            x_value = sorted_group.iloc[year_i]["Weekly_Sales"]
+            size_value = sorted_group.iloc[year_i]["Size"]
+            isholiday_value = sorted_group.iloc[year_i]["IsHoliday"]
+            fuel_price_value = sorted_group.iloc[year_i]["Fuel_Price"]
+            cpi_value = sorted_group.iloc[year_i]["CPI"]
+            unempl_value = sorted_group.iloc[year_i]["Unemployment"]
+            week_n_value = sorted_group.iloc[year_i]["week_n"]
+            md1_value = sorted_group.iloc[year_i]["MarkDown1"]
+            md2_value = sorted_group.iloc[year_i]["MarkDown2"]
+            md3_value = sorted_group.iloc[year_i]["MarkDown3"]
+            md4_value = sorted_group.iloc[year_i]["MarkDown4"]
+            md5_value = sorted_group.iloc[year_i]["MarkDown5"]
+
+            transformed_data_row[year_n_label] = year_value
+            transformed_data_row[year_n_sales_label] = x_value
+            transformed_data_row[size_n_label] = size_value
+            transformed_data_row[isholiday_n_label] = isholiday_value
+            transformed_data_row[fuel_price_n_label] = fuel_price_value
+            transformed_data_row[cpi_n_label] = cpi_value
+            transformed_data_row[unempl_n_label] = unempl_value
+            transformed_data_row[week_n_n_label] = week_n_value
+            transformed_data_row[md1_n_label] = md1_value
+            transformed_data_row[md2_n_label] = md2_value
+            transformed_data_row[md3_n_label] = md3_value
+            transformed_data_row[md4_n_label] = md4_value
+            transformed_data_row[md5_n_label] = md5_value
+
+        transformed_data.append(transformed_data_row)
+
+    return pd.DataFrame(transformed_data)
+
+def format_wm_data_colnames(data, dataset_name):
+    print("Total groups: ", len(data.drop_duplicates(["wm_date", "store_dept"])))
+    print(len(data))
+    data = data.rename({"Store": "Store" + "_" + dataset_name,
+                        "Dept": "Dept" + "_" + dataset_name,
+                        "Date": "Date" + "_" + dataset_name,
+
+                        "year0": "year0" + "_" + dataset_name,
+                        "year0_sales": "year0_sales" + "_" + dataset_name,
+                        "year0_size": "year0_size" + "_" + dataset_name,
+                        "year0_isholiday": "year0_isholiday" + "_" + dataset_name,
+                        "year0_fuel_price": "year0_fuel_price" + "_" + dataset_name,
+                        "year0_cpi": "year0_cpi" + "_" + dataset_name,
+                        "year0_unempl": "year0_unempl" + "_" + dataset_name,
+                        "year0_week_n": "year0_week_n" + "_" + dataset_name,
+                        "year0_md1": "year0_md1" + "_" + dataset_name,
+                        "year0_md2": "year0_md2" + "_" + dataset_name,
+                        "year0_md3": "year0_md3" + "_" + dataset_name,
+                        "year0_md4": "year0_md4" + "_" + dataset_name,
+                        "year0_md5": "year0_md5" + "_" + dataset_name,
+
+                        "year1": "year1" + "_" + dataset_name,
+                        "year1_sales": "year1_sales" + "_" + dataset_name,
+                        "year1_size": "year1_size" + "_" + dataset_name,
+                        "year1_isholiday": "year1_isholiday" + "_" + dataset_name,
+                        "year1_fuel_price": "year1_fuel_price" + "_" + dataset_name,
+                        "year1_cpi": "year1_cpi" + "_" + dataset_name,
+                        "year1_unempl": "year1_unempl" + "_" + dataset_name,
+                        "year1_week_n": "year1_week_n" + "_" + dataset_name,
+                        "year1_md1": "year1_md1" + "_" + dataset_name,
+                        "year1_md2": "year1_md2" + "_" + dataset_name,
+                        "year1_md3": "year1_md3" + "_" + dataset_name,
+                        "year1_md4": "year1_md4" + "_" + dataset_name,
+                        "year1_md5": "year1_md5" + "_" + dataset_name}, axis=1)
+
+    return data
+
+def dummy_fill_store_dept_median(row, refference_data, fill_colnames):
+    store_dept_data = refference_data[refference_data["store_dept"] == row["store_dept"]]
+    for fill_colname in fill_colnames:
+        row[fill_colname] = store_dept_data[fill_colname].median()
+    return row
+
+def dummy_fill_store_median(row, refference_data, fill_colnames):
+    store_data = refference_data[refference_data["Store_train"] == row["Store_test"]]
+    for fill_colname in fill_colnames:
+        row[fill_colname] = store_data[fill_colname].median()
+    return row
